6991f9a1df6ebf8d4b80f6db77d199c2f62d2dcd
kapit4n <luis.arce22@gmail.com>
2019-04-15 15:57:27 -0400

    Updated rooms service to serve more data

--- src/comps/rooms/RoomView.js
+++ src/comps/rooms/RoomView.js
@@ -11,6 +11,7 @@ export default function RoomView(props) {
   
   return (
     <div>
+    Example
     <h1>{roomInfo.name}</h1>
     <img src={roomInfo.img} style={{width: '400px'}}/>
     <p>
--- src/services/RoomsService.js
+++ src/services/RoomsService.js
@@ -1,27 +1,48 @@
 const roomsList = [
   {
-    id: 1, name: 'Room 1', 
-    img: "https://s-ec.bstatic.com/images/hotel/max1024x768/731/73118462.jpg",
+    id: 1, name: 'Single Room',
+    img: "https://www.thonhotels.com/siteassets/hoteller/norge/oslo/thon-hotel-terminus/romtyper/small-single-room/thon-hotel-terminus-small-single-room-1.jpg?width=1100&height=550&mode=crop&quality=80",
     description: `The room comes with Dreamax bed (manufactured and 
     designed exclusively by Flex for Meliá Hotels International), a modern, 
     fully equipped bathroom finished ...`
-  },{
-    id: 2, name: 'Room 2', 
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI_QdkWDQ14NeiuWrC8B6rIBTxOKtz_aapSiLNSJuXZGPj17h_",
+  }, {
+    id: 2, name: 'Double Room',
+    img: "https://media-cdn.tripadvisor.com/media/photo-s/05/6f/52/8c/doubletree-by-hilton.jpg",
+    description: `The room comes with Dreamax bed (manufactured and designed 
+    exclusively by Flex for Meliá Hotels International), a modern, fully equipped bathroom finished ...`
+  }, {
+    id: 3, name: 'Tiple Room',
+    img: "https://fishh.nnhotels.com/files/img/small/triple-room-hotel-u232-2-524.jpg",
+    description: `The room comes with Dreamax bed (manufactured and designed 
+    exclusively by Flex for Meliá Hotels International), a modern, fully equipped bathroom finished ...`
+  }, {
+    id: 4, name: 'Quad Room',
+    img: "https://www.viewhotels.co.jp/asakusa/english/accommondations/images/fourth_img01.jpg",
+    description: `The room comes with Dreamax bed (manufactured and designed 
+    exclusively by Flex for Meliá Hotels International), a modern, fully equipped bathroom finished ...`
+  }, {
+    id: 5, name: 'Queen Room',
+    img: "https://www.schloesslehotel.com/wp-content/uploads/2016/07/Superior-Garden-Queen-2.jpg",
+    description: `The room comes with Dreamax bed (manufactured and designed 
+    exclusively by Flex for Meliá Hotels International), a modern, fully equipped bathroom finished ...`
+  }, {
+    id: 6, name: 'King Room',
+    img: "https://www.mgmresorts.com/content/dam/MGM/bellagio/hotel/resort-room-king-sapphire/architecture/bellagio-hotel-resort-room-king-sapphire.tif",
     description: `The room comes with Dreamax bed (manufactured and designed 
     exclusively by Flex for Meliá Hotels International), a modern, fully equipped bathroom finished ...`
   }
-
 ]
 
 const RoomsService = {
-    getRooms: function() {
-      return roomsList;
-    },
+  getRooms: function () {
+    return roomsList;
+  },
 
-    getRoomById: function(id) {
-      return roomsList.filter(x => x.id == id);
-    }
+  getRoomById: function (id) {
+    let selRoom = roomsList.find(x => x.id == id);
+    console.log(selRoom);
+    return selRoom;
+  }
 };
 
 export default RoomsService;
