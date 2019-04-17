1861a0455afc40b84aa4423bc46fbcd54b6e0f53
kapit4n <luis.arce22@gmail.com>
2019-04-17 14:56:02 -0400

    Change to react-bootstrap beta version

--- src/comps/rooms/RoomView.js
+++ src/comps/rooms/RoomView.js
@@ -1,23 +1,28 @@
 import React, { useState } from "react";
 import RoomsService from './../../services/RoomsService';
+import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export default function RoomView(props) {
 
   const [roomInfo, setRoomInfo] = useState(RoomsService.getRoomById(props.match.params.id))
 
-  console.log(props);
-
-  const { match: { params } } = props;
-
   return (
-    <div>
-      <h1>{roomInfo.name}<Link to={`/rooms-edit/${roomInfo.id}`}>edit</Link></h1>
-      <img src={roomInfo.img} style={{ width: '500px' }} />
-      <p>
-        {roomInfo.description}
-      </p>
-    </div>
+    <Card style={{ width: '30rem' }}>
+      <Card.Img variant="top" src={roomInfo.img} />
+      <Card.Body>
+        <Card.Title>{roomInfo.name}</Card.Title>
+        <Card.Text>
+          {roomInfo.description}
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroupItem>Feature 1</ListGroupItem>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href={`/rooms-edit/${roomInfo.id}`}>Card Edit</Card.Link>
+      </Card.Body>
+    </Card>
   )
 
 }
