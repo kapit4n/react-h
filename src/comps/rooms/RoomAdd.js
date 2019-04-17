59ca98ba55802008d18d644a9fda9cc9c6de51a8
kapit4n <luis.arce22@gmail.com>
2019-04-17 14:22:42 -0400

    Add new room page

--- src/comps/rooms/RoomAdd.js
+++ src/comps/rooms/RoomAdd.js
@@ -2,10 +2,16 @@ import React, { useState } from "react";
 
 export default function RoomAdd() {
 
-  const [roomInfo, setRoomInfo] = useState("This is the room info")
+  const [name, setName] = useState("");
+  const [img, setImg] = useState("https://i.dmarge.com/2014/04/pinterest-ideas-bedrooms-rhpinterestcouk-best-Blue-Bedroom-Bachelor-bedroom-images-on-pinterest-ideas-bedrooms-and-rhpinterestcouk-bachelorus-pad-for-young-energetic-men-1100x880.jpg");
+  const [description, setDescription] = useState("");
+  
   return (
     <div>
-      {roomInfo}
+      <input onChange={(e) => setName(e.target.value)} placeholder="Name"/>
+      <input onChange={(e) => setImg(e.target.value)} placeholder="http://"/>
+      <img src={img} style={{width: 450}}/>
+      <textarea onChange={(e) => setDescription(e.target.value)} placeholder="Description" cols="46"/>
     </div>
   )
 
