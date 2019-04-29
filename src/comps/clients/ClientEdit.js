24cffbcaeaeafb31a872cc1ff0483e744275b96f
Luis Arce <luis.arce22@gmail.com>
2019-04-28 20:44:29 -0400

    Fixed client eddit page

--- src/comps/clients/ClientEdit.js
+++ src/comps/clients/ClientEdit.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Card, Form, ButtonToolbar, Button } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 import ClientsService from './../../services/ClientsService';
 
 export default function ClientEdit(props) {
@@ -10,17 +10,16 @@ export default function ClientEdit(props) {
       <Form>
         <Form.Group controlId="name">
           <Form.Label>Name</Form.Label>
-          <Form.Control value={clientInfo.duration} onChange={(e) => setClientInfo(Object.assign({}, clientInfo, { name: e.target.value }))} type="text" placeholder="Name" />
+          <Form.Control value={clientInfo.name} onChange={(e) => setClientInfo(Object.assign({}, clientInfo, { name: e.target.value }))} type="text" placeholder="Name" />
         </Form.Group>
         
         <Form.Group controlId="identifier">
           <Form.Label>Identifier</Form.Label>
-          <Form.Control value={clientInfo.duration} onChange={(e) => setClientInfo(Object.assign({}, clientInfo, { identifier: e.target.value }))} type="text" placeholder="Indentifier" />
+          <Form.Control value={clientInfo.identifier} onChange={(e) => setClientInfo(Object.assign({}, clientInfo, { identifier: e.target.value }))} type="text" placeholder="Indentifier" />
         </Form.Group>
         <Button variant="primary" type="submit">
           Save
         </Button>
       </Form>
   )
-
 }
