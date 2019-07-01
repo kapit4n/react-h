17fb13960919c1bcb1961047bed754cb69db27df
Luis Arce <luis.arce22@gmail.com>
2019-06-30 23:21:17 -0400

    feat(home): collapse on select

--- src/index.jsx
+++ src/index.jsx
@@ -42,7 +42,7 @@ class App extends Component {
 
   render() {
     return (
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="light" expand="lg" collapseOnSelect>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
