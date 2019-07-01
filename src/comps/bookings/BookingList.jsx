6bed7eda4b0192df9067172668067d36cc8771ef
Luis Arce <luis.arce22@gmail.com>
2019-06-30 23:14:18 -0400

    feat(home): mobile support

--- src/comps/bookings/BookingList.jsx
+++ src/comps/bookings/BookingList.jsx
@@ -1,41 +1,47 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import BookingsService from '../../services/BookingsService';
-import { Table } from 'react-bootstrap';
+import { Table, Button, ListGroup } from 'react-bootstrap';
 
 export default function BookingList() {
 
   const [bookingList, setBookingList] = useState(BookingsService.getBookings());
   return (
-
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>User</th>
-          <th>Date</th>
-          <th>Duration</th>
-          <th>Payed</th>
-          <th>Total</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {bookingList.map(r => (
-          <tr key={r.id}>
-            <td>{r.id}</td>
-            <td>{r.user.name}</td>
-            <td>{r.startDate}</td>
-            <td>{r.duration}</td>
-            <td>{r.payed}</td>
-            <td>{r.totalToPay}</td>
-            <td>
-              <Link className="btn btn-primary" to={`/bookings-info/${r.id}`}>Open
-            </Link>
-            </td>
+    <>
+      <Table striped bordered hover className="d-none d-sm-block">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>User</th>
+            <th>Date</th>
+            <th>Duration</th>
+            <th>Payed</th>
+            <th>Total</th>
+            <th></th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {bookingList.map(r => (
+            <tr key={r.id}>
+              <td>{r.id}</td>
+              <td>{r.user.name}</td>
+              <td>{r.startDate}</td>
+              <td>{r.duration}</td>
+              <td>{r.payed}</td>
+              <td>{r.totalToPay}</td>
+              <td>
+                <Link className="btn btn-primary" to={`/bookings-info/${r.id}`}>Open
+            </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <ListGroup className=".d-block .d-sm-none">
+      {bookingList.map(r => (
+        <ListGroup.Item key={r.id}>{r.user.name}: {r.startDate}</ListGroup.Item>
+      )) }
+      </ListGroup>
+    </>
   )
 }
